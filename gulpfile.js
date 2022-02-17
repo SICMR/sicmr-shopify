f0424ab44e429c25e7c5bf076d16dde1faead612
Brendan Sheehan <15129375+feemcgill@users.noreply.github.com>
2022-02-17 15:13:00 -0800

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,14 @@ var rename = require('gulp-rename')
 var sass = require('gulp-sass')(require('sass'))
 var autoprefixer = require('gulp-autoprefixer')
 var sourcemaps = require('gulp-sourcemaps')
-var connect = require('gulp-connect')
-var cors = require('cors')
 
 // COMPILE SASS
 gulp.task('sass', function () {
-  return gulp
-    .src('scss/index.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(rename('assets/styles.css'))
-    .pipe(gulp.dest('./'))
-    .pipe(connect.reload())
-})
-
-gulp.task('server', function () {
-  connect.server({
-    port: 6969,
-    host: 'localhost',
-    root: './',
-    debug: true,
-    livereload: true,
-    middleware: (connect, opt) => {
-      return [cors()]
-    },
-  })
+  return gulp.src('scss/index.scss').pipe(sass().on('error', sass.logError)).pipe(autoprefixer()).pipe(rename('assets/styles.css')).pipe(gulp.dest('./'))
 })
 
 gulp.task('watch', function () {
   gulp.watch(['scss/*', 'scss/**/*'], gulp.series('sass'))
 })
 
-gulp.task('default', gulp.parallel('server', 'watch'))
+gulp.task('default', gulp.parallel('watch'))
